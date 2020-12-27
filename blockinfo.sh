96220bbae281057aafda95a6ee44d26806d4a752
ghost <Dillonwu999@gmail.com>
2020-12-26 21:32:07 -0500

    blockchain info script

---
+++ blockinfo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script gets information about a specific bitcoin address
+# Tools used: curl
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <address>"
+	exit 1
+fi
+
+base_url="https://blockchain.info/q/"
+
+balance=$(curl -s $base_url"/addressbalance/"$1)
+recv=$(curl -s $base_url"/getreceivedbyaddress/"$1)
+sent=$(curl -s $base_url"/getsentbyaddress/"$1)
+first_made=$(curl -s $base_url"/addressfirstseen/"$1)
+
+echo "Balance: "$balance
+echo "Receive By Address: $recv"
+echo "Set By Address: $sent"
+echo "First seen: $(date -d @$first_made)"
+
