e302c06eac61083ff7c3563b7f8f4c4e69405c4f
Dillon Wu <dillonw@andrew.cmu.edu>
2023-02-07 21:55:45 -0800

    setup v0.0 ready

--- bootstrap-linux/bootstrap.sh
+++ bootstrap-linux/bootstrap.sh
@@ -11,7 +11,7 @@
 
 if [ -z "$1" ]
 then
-	echo "No user supplied"
+	echo "No users supplied"
 	exit 1
 fi
 
@@ -20,35 +20,13 @@ DIR=$(pwd)
 BASHRC=~/.bashrc
 ZSHRC=~/.zshrc
 
-
 # Installing the necessary system packages
-#apt-get -y upgrade
-#apt-get install -y aptitude
-#cat $DIR/system-req.txt | xargs aptitude install -y
+apt-get -y upgrade
+apt-get install -y aptitude
+cat $DIR/system-req.txt | xargs aptitude install -y
 
-# Installing oh-my-zsh
+# Installing python scripts
+python3 -m pip install -r $DIR/python-req.txt
 
-#sudo -u ${1} sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-#sudo -u ${1} git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-#echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-ZSH_CMDS=("wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -"
-"git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k"
-"echo \"source ~/powerlevel10k/powerlevel10k.zsh-theme\" >>~/.zshrc"
-)
-for cmd in "${ZSH_CMDS[@]}"
-do
-	sudo -u ${1} bash -c "${cmd}"
-done
-
-
-# Configuring bashrc file and zshrc file
-# For each command, do for ~/.bashrc file and ~/.zshrc file
-# Create an array of commands maybe
-
-#echo 'alias python=python' >> ~/.bashrc
-
-
-
-# TODO: Modify the ~/bashrc file so that it contains all of the correct aliases
-# TODO: Place the correct configuration files in the right directory - tmux, neovim
-# Get rid of the beep sound
+# Running script as user to set up environment
+sudo -u ${1} $DIR/user-script.sh
---
+++ bootstrap-linux/user-script.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+##################################################
+# This script executes the following commands as the default user
+##################################################
+DIR=$(pwd)
+
+# Install zshrc
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# Moving the neovim configuration files to the correct location
+cp $DIR/init.vim ~/.config/nvim/init.vim
+# Moving the tmux configuration files to the correct location
+cp $DIR/tmux.conf ~/.tmux.conf
+
+# Moving certain commands into zsh source file
+ECHO_ARR=( "source ~/powerlevel10k/powerlevel10k.zsh-theme"
+"alias vi=nvim"
+"alias vim=nvim"
+"alias python=python3"
+"alias cdd=\'cd Desktop\'"
+"alias dk=docker" )
+
+for i in ${ECHO_ARR[@]}
+do 
+	echo ${i} >> ~/.zshrc
+done
+
