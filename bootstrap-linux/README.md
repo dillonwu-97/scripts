495cceb2c2f360f5d21c9d1973fd54f63a6d8a72
Dillon Wu <dillonw@andrew.cmu.edu>
2023-02-07 21:09:20 -0500

    bootstrap starter

---
+++ bootstrap-linux/README.md
@@ -0,0 +1,1 @@
+This is the directory for bootstrapping a new Ubuntu OS
---
+++ bootstrap-linux/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+sudo apt-get upgrade
+
+sudo apt-get install python3 
+
+
+#########################
+# Includes:
+#   docker
+#   pwntools
+#   qemu
+#   numpy
+#   tmux
+#   ohmyzsh
+#   nvim
+#########################
