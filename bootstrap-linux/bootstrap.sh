a9c18f55f93551f01394ce828435722d4790cef3
Dillon Wu <dillonw@andrew.cmu.edu>
2024-01-05 20:27:31 -0500

    adding cleanup functionality

--- bootstrap-linux/bootstrap.sh
+++ bootstrap-linux/bootstrap.sh
@@ -24,7 +24,7 @@ debug=false
 while getopts 'f' flag; do
     case "${flag}" in
         f) debug=true ;;
-        *) error "Unrecognized flag" ;;
+        *) echo "Unrecognized flag" && exit 1 ;;
     esac
 done
 
@@ -32,11 +32,15 @@ done
 # if we are in debug mode, just remove everything and then rerun the script
 if [[ "$debug" = true ]]; then
     echo "Debug mode"
+    rm /etc/apt/keyrings/docker.gpg
+    cat $DIR/system-req.txt | xargs aptitude uninstall -y
+    python3 -m pip uninstall -r $DIR/python-req.txt
 fi
 
 
 # Installing docker prerequisites
 # TODO: Maybe I need to check that this actually works as intended, like is there an if-else check that needs to happen here?
+echo "[*] Grabbing docker gpg key"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
@@ -44,20 +48,20 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Installing the necessary system packages
+echo "[*] Installing system packages" 
 apt-get -y upgrade
 apt-get install -y aptitude
 cat $DIR/system-req.txt | xargs aptitude install -y
 
 
 # Installing python scripts
+echo "[*] Installing python packages"
 python3 -m pip install -r $DIR/python-req.txt
-
-# Running script as user to set up environment
-#sudo -u ${1} $DIR/user-script.sh
 
 # One more upgrade
 apt-get -y upgrade
 
 # Setting up the configuration
+echo "[*] Copying configuration files"
 cp -r $DIR/nvim/ $HOME/.config/nvim/
 cp -r $DIR/zsh/ $HOME/.config/zsh
